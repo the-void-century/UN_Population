ac2d6d588e400fa4533eb15b8afe514822f0812b
the-void-century <utsdhall@gmail.com>
2023-04-26 10:08:15 +0530

    Completed all modules

--- UN_Analytics.py
+++ UN_Analytics.py
@@ -24,13 +24,16 @@ def calculate():
                     SAARC_COUNTRY_TO_POPULATION[population["Region"]]=float(population["Population"])
                 else:
                     SAARC_COUNTRY_TO_POPULATION[population["Region"]]+=float(population["Population"])
-            if population["Region"] in ASEAN_COUNTRIES:
-                if population["Year"] not in ASEAN_BY_YEAR:
-                    country_to_population={population["Region"]:float(population["Population"])}
-                    ASEAN_BY_YEAR[population["Year"]]=country_to_population
-                else:
-                    if population["Region"] not in ASEAN_BY_YEAR[population["Year"]]:
-                        ASEAN_BY_YEAR[population["Year"]][population["Region"]]=float(population["Population"])
+            start="2004"
+            end="2014"        
+            if population["Year"]>=start and population["Year"]<=end:                     
+                if population["Region"] in ASEAN_COUNTRIES:
+                    if population["Year"] not in ASEAN_BY_YEAR:
+                        country_to_population={"Brunei Darussalam":0,"Cambodia":0,"Indonesia":0,"Malaysia":0,"Myanmar":0,"Phillipenes":0,"Singapore":0,"Thailand":0,"Vietnam":0}
+                        country_to_population[population["Region"]]=float(population["Population"])
+                        ASEAN_BY_YEAR[population["Year"]]=country_to_population
+                    else:
+                        ASEAN_BY_YEAR[population["Year"]][population["Region"]]+=float(population["Population"])
                 
 
 
@@ -41,6 +44,9 @@ def india_population_over_years():
     year,population=zip(*sorted(zip(year,population)))
     plt.bar(year,population,color='maroon')
     plt.xticks(rotation=40)
+    plt.ylabel("Population")
+    plt.xlabel("Years")
+    plt.title("Population growth of India over the years")
     plt.show()
 
 def asean_population():
@@ -49,6 +55,9 @@ def asean_population():
     population=list(ASEAN_COUNTRY_TO_POPULATION.values())
     plt.bar(country,population,color='maroon')
     plt.xticks(rotation=30)
+    plt.xlabel("Countries")
+    plt.ylabel("Populations")
+    plt.title("Population of ASEAN countries in 2014")
     plt.show()
 
 def saarc_population():
@@ -56,6 +65,9 @@ def saarc_population():
     country=list(SAARC_COUNTRY_TO_POPULATION.keys())
     population=list(SAARC_COUNTRY_TO_POPULATION.values())
     plt.bar(country,population,color='maroon')
+    plt.xlabel("Countries")
+    plt.ylabel("Population")
+    plt.title("The population of saarc countries in all years")
     plt.xticks(rotation=30)
     plt.show()
 
@@ -64,13 +76,21 @@ def asean_population_over_year():
     """This function plots a grouped bar chart of ASEAN countries over the years"""
     color_count=0
     Years=list(ASEAN_BY_YEAR.keys())
-    for years in ASEAN_BY_YEAR:
-        country=list(ASEAN_BY_YEAR[years].keys())
-        population=list(ASEAN_BY_YEAR[years].values())
-        country,population=zip(*sorted(zip(country,population)))
-        plt.bar(country,population,width=0.2)
-    plt.xticks(Years,rotation=45)
-    plt.legend(ASEAN_COUNTRIES)
+    width=0.10
+    k=-5
+    dynamic_width=[i for i in range(9)]
+    print(ASEAN_BY_YEAR)
+    for countries in ASEAN_BY_YEAR:
+        population=list(ASEAN_BY_YEAR[countries].values())
+        print(population)
+        plt.bar([i+(k*width) for i in dynamic_width],population,width)
+        k+=1
+    plt.xticks(rotation=45)
+    plt.xticks(dynamic_width,ASEAN_COUNTRIES)
+    plt.xlabel("Years")
+    plt.ylabel("Population")
+    plt.title("Grouped bar plot showcasing the growth of ASEAN countries over the years")
+    plt.legend(Years)
     plt.show()
 
 
